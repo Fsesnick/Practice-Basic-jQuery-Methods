03aa419961ab9ce46a9db8e5ca89a9788c811a0c
Fsesnick <fernandasesnick@gmail.com>
2021-02-02 12:44:34 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// 1. Mostre a crítica oculta do restaurante ou use um método de animação para revelá-la.
+
+// 2. Defina o título da crítica oculta do restaurante com um nome diferente - o que você quiser.
+
+// 3. Altere o texto da crítica oculta do restaurante. A revisão deve conter HTML, como uma tag strong ou um elemento span.
+
+// 4. Altere o nome do aplicativo. Altere * apenas * o texto dentro do elemento span dentro de h1.
+
+// 5. Crie uma animação para o parágrafo com a id de "flashMessage." Encadeie uma animação que
+
+  // desliza a mensagem para baixo por 1 segundo
+  // atrasa a mensagem por 2 segundos
+  // desliza a mensagem para cima por 1 segundo
+
+// 6. Dentro do manipulador de cliques, crie um formulário funcional de avaliação do restaurante.
+$ ('# submitBtn'). click (function () {
+  //  uma. Use o método val () para obter o que foi inserido nos campos de entrada do nome do restaurante e do formulário de avaliação do restaurante. Salve ambos os valores em variáveis.
+  // b. Insira o valor do nome do restaurante na tag #newRestaurant h3
+  // c. Insira o valor da avaliação do restaurante na tag de parágrafo #newRestaurant
+}); 
